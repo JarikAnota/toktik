4d76adeb0a439c878de6d9cd8a4491661b7d26c8
JarikAnota <jakssanota@gmail.com>
2024-01-03 19:21:58 -0600

    first commit

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,54 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Admirando el mar',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 3927,
+    'views': 139183,
+  },
+  {
+    'name': 'El pianista',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 31312,
+    'views': 928392,
+  },
+  {
+    'name': 'Titanic de papel',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 91813,
+    'views': 938981,
+  },
+  {
+    'name': 'Feliz 2024',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 129182,
+    'views': 983918391,
+  },
+  {
+    'name': 'La mejor ciudad',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 2322,
+    'views': 183772,
+  },
+  {
+    'name': 'Bajo la estrella de mi ser',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 23982,
+    'views': 9183918,
+  },
+  {
+    'name': 'Leer expande la luz del alma',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': 'Alfin con mi xbox series s',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 3342,
+    'views': 339232,
+  },
+
+
+];
+
+
